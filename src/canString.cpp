48ca7fae88a49c9c4c53a9445c3b84942112ee16
admin <BMValeev@stud.kai.ru>
2023-03-12 19:14:09 +0300

    Перенес проверки в точку входных параметров.

--- src/canString.cpp
+++ src/canString.cpp
@@ -1,17 +1,30 @@
 //
 // Created by gyxma on 10.03.2023.
 //
-
+//" (1970-01-01 05:36:04.335344)  can0  604   [8]  40 18 10 03 00 00 00 00   '@.......'"
 #include "canString.h"
-canString::canString(std::string data) {
-    mBaseline=data;
+canString::canString(const std::string &data)
+    :mBaseline(data) {
     mCorrect=parse();
 }
 
 bool canString::parse() {
-    std::size_t found;
+    std::string processedLine=mBaseline;
+    if(findParameter(&processedLine,"date","(",")"))
+        return false;
+    if(!findParameter(&processedLine,"can"))
+        return false;
+    if(!findParameter(&processedLine,"node"))
+        return false;
+    if(!findParameter(&processedLine,"type"))
+        return false;
+    if(!findParameter(&processedLine,"hex"," ","\'"))
+        return false;
+    if(!findParameter(&processedLine,"string","\'","\'"))
+        return false;
+    return true;
 }
-bool canString::checkAddress(std::vector<std::string> address){
+/*bool canString::checkAddress(std::vector<std::string> address){
     if (!isCorrect())
         return false;
     for(auto i: address){
@@ -19,4 +32,44 @@ bool canString::checkAddress(std::vector<std::string> address){
             return true;
     }
     return false;
+}*/
+bool canString::parseNodeId(const std::string &value ){
+    if(value.size()==8){
+        if((value.substr(0,3)!="1E0")
+            ||(value.substr(3,1).find_first_not_of("01") != std::string::npos)
+            ||(value.substr(4).find_first_not_of("0123456789abcdefABCDEF") != std::string::npos)
+            )
+            return false;
+        mNodeId="0x00";
+        mNodeId[2]=value[4];
+        mNodeId[3]=value[5];
+    }
+    if(value.size()==3){
+         if(value.find_first_not_of("0123456789abcdefABCDEF", 3) != std::string::npos)
+             return false;
+        mNodeId="0x00";
+        mNodeId=value.substr(1,2);
+    }
+    return true;
+}
+
+bool canString::findParameter(std::string* value,const std::string & name,const std::string &startSymbol,const std::string &endSymbol){
+    std::size_t foundParStart,foundParEnd;
+    if (startSymbol==" ")
+        foundParStart = value->find_first_not_of(startSymbol);
+    else
+        foundParStart = value->find_first_of(startSymbol);
+    if (foundParStart==std::string::npos)
+        return false;
+    foundParEnd = value->substr(foundParStart).find_first_of(endSymbol);
+    if (foundParEnd==std::string::npos)
+        return false;
+    if (foundParStart==foundParEnd)
+        return false;
+    parameters[name]=value->substr(foundParStart,foundParEnd-foundParStart);
+    std::string temp=value->substr(foundParEnd);
+    if (!temp.size())
+        return false;
+    value->swap(temp);
+    return true;
 }
--- src/canString.h
+++ src/canString.h
@@ -5,24 +5,31 @@
 #ifndef CANTRANSLATOR_CANSTRING_H
 #define CANTRANSLATOR_CANSTRING_H
 
-
+//" (1970-01-01 05:36:04.335344)  can0  604   [8]  40 18 10 03 00 00 00 00   '@.......'"
 #include "string"
+#include "map"
 #include "vector"
 class canString {
 public:
-    explicit canString(std::string data);
+    explicit canString(const std::string &data);
     ~canString()=default;
     std::string getString()     {   return mBaseline;   }
     std::string getAddress()    {   return mNodeId;     } //возвращает значение  адреса в формате строки без проверок.
     bool isCorrect()            {   return mCorrect;    }
 protected:
+    //Распарсить параметры в строке по типу данных и провести базовую проверку
     bool parse();
-    bool checkAddress(std::vector<std::string> address);
+    /*bool checkAddress(std::vector<std::string> address);*/
 
 private:
+    //сохранить адрес получателя в формате в котором будет происходить проверка
+    bool parseNodeId(const std::string &value );
+    //Парсинг строки по конкретному параметру и фильтрация по символам, для упрощения чтения кода
+    bool findParameter(std::string* value,const std::string & name,const std::string &startSymbol=" ",const std::string &endSymbol=" ");
     std::string mBaseline;
-    std::string mDate,mCan,mNodeId;
+    std::string mNodeId;
     bool mCorrect;
+    std::map<std::string,std::string> parameters;
 };
 
 #endif //CANTRANSLATOR_CANSTRING_H
